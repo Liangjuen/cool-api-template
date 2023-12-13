bb905622f11de64c1ec96738ec4859317b39d74e
梁举恩 <3103421940@qq.com>
2023-12-13 21:28:29 +0800

    typeorm 配置实现

--- src/core/config/database.config.ts
+++ src/core/config/database.config.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata'
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { Logger } from '@services'
+import { TYPEORM } from './env.config'
 
-export class DatabaseConfiguration {
+class DatabaseConfiguration {
 	/**
 	 * @description 数据源
 	 */
@@ -24,3 +25,22 @@ export class DatabaseConfiguration {
 		}
 	}
 }
+// typeorm 数据库连接配置 可查阅: https://typeorm.biunav.com/zh/
+const DataBase = new DatabaseConfiguration({
+	type: TYPEORM.TYPE as 'mysql',
+	host: TYPEORM.HOST,
+	port: TYPEORM.PORT,
+	username: TYPEORM.USER,
+	password: TYPEORM.PWD,
+	database: TYPEORM.DB,
+	entities: [TYPEORM.ENTITIES],
+	subscribers: [TYPEORM.SUBSCRIBERS],
+	synchronize: TYPEORM.SYNC,
+	logging: TYPEORM.LOG,
+	cache: TYPEORM.CACHE
+})
+
+console.log(TYPEORM.ENTITIES)
+
+export const DateSource = DataBase.source
+export default DataBase
