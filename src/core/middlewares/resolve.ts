a44ad14efd83410281017af37b2195e130415377
梁举恩 <3103421940@qq.com>
2023-12-15 21:05:48 +0800

    新增 resolve 中间件

---
+++ src/core/middlewares/resolve.ts
@@ -0,0 +1,54 @@
+import { Request } from 'express'
+import { IResponse } from '@interfaces'
+import { HttpResponseStatusCode as HttpCode } from '@enums'
+import { getStatusCode } from '@utils/http'
+import { CacheService } from '@services/Cache'
+
+export default class Resolve {
+	/**
+	 * @description 解析当前请求并获取输出(除开404错误)
+	 * @param req Express Request 的派生对象
+	 * @param res Express Response 的派生对象
+	 * @param next 回调函数
+	 * @returns
+	 */
+	static async output(req: Request, res: IResponse, next: (e?: Error) => void) {
+		const hasContent = typeof res.locals?.data !== 'undefined'
+		const hasNullContent = res.locals.data === null
+		const hasStatusCodeOnResponse = typeof res.statusCode !== 'undefined'
+		const status = getStatusCode(req.method, hasContent && !hasNullContent)
+
+		if (req.method === 'DELETE') {
+			res.status(status)
+			res.end()
+			return
+		}
+
+		if (!hasContent) next()
+
+		if (
+			(hasContent && ['GET', 'POST', 'PUT', 'PATCH'].includes(req.method)) ||
+			(hasStatusCodeOnResponse && res.statusCode !== HttpCode.NotFound)
+		) {
+			//  缓存当前请求
+			if (CacheService.isCachable(req)) {
+				// 仅缓存数组类型
+				if (Array.isArray(res.locals.data)) {
+					const cacheKey = CacheService.key(req)
+					await CacheService.setList(cacheKey, res.locals.data)
+					const others = {
+						code: res.locals.code,
+						msg: res.locals.msg,
+						meta: res.locals.meta
+					}
+					await CacheService.set(
+						CacheService.metaKey(cacheKey),
+						JSON.stringify(others)
+					)
+				}
+			}
+			res.status(status)
+			res.json(res.locals)
+		}
+	}
+}
