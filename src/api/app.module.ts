e0f8fd2653c390ecc57ff9c5b50460e5eedd6838
梁举恩 <3103421940@qq.com>
2023-12-17 17:30:44 +0800

    新增模块

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,15 +1,19 @@
-import { IAppRoute } from '@interfaces'
 import { AppModule as Module } from '@classes'
-
-import { UserRouter } from '@api/modules/user/user.router'
+import { IAppRoute } from '@interfaces'
+import { UserRouter } from './modules/base/user/user.router'
+import { DictRouter } from './modules/dict/info/dict.router'
+import { DictTypeRouter } from './modules/dict/type/router'
 
 export class AppModule extends Module {
-	routes: IAppRoute[] = [
-		{ segment: '/v1/users', provider: UserRouter, version: '1.0.0' }
-	]
+	routes(): IAppRoute[] {
+		return [
+			{ segment: '/v1/users', provider: UserRouter, version: 'v1' },
+			{ segment: '/v1/dicts', provider: DictRouter, version: 'v1' },
+			{ segment: '/v1/dictTypes', provider: DictTypeRouter, version: 'v1' }
+		]
+	}
 
 	constructor() {
 		super()
-		this.mapRoute()
 	}
 }
