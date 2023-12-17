3995095408ebc53b5e3d54b8b5991d63d7b51f68
梁举恩 <3103421940@qq.com>
2023-12-17 17:30:20 +0800

    更新挂载路由逻辑

--- src/core/classes/router.class.ts
+++ src/core/classes/router.class.ts
@@ -1,5 +1,5 @@
-import { Router, RequestHandler } from 'express'
-import { IAppRoute, IRoute, IRouteModule } from '@interfaces'
+import { Router } from 'express'
+import { IAppRoute, IRoute } from '@interfaces'
 
 /**
  * Router 基类
@@ -12,44 +12,36 @@ export abstract class BaseRouter {
 	constructor() {
 		this.router = Router()
 	}
-
-	mapRoute(): void {}
 }
 
 /**
  *@description 主路由
  */
 export abstract class AppModule extends BaseRouter {
-	abstract routes: IAppRoute[]
+	/**
+	 * @description 路由集合
+	 */
+	abstract routes(): IAppRoute[]
 	constructor() {
 		super()
-	}
-
-	mapRoute(): Router {
-		if (!this.routes || !this.routes.length) return this.router
-		this.routes.forEach(route => {
+		this.routes().forEach(route => {
 			const instance = new route.provider()
 			this.router.use(route.segment, instance.router)
 		})
-		return this.router
 	}
 }
 
 /**
  * @description 模块路由
  */
-export abstract class RouteModule extends BaseRouter implements IRouteModule {
-	service: string
-	name: string
-	routes: IRoute[]
-	public middlewares: RequestHandler[]
+export abstract class RouteModule extends BaseRouter {
+	/**
+	 * @description 路由集合
+	 */
+	abstract routes(): IRoute[]
 	constructor() {
 		super()
-	}
-
-	mapRoute(): void {
-		if (!this.routes || !this.routes.length) return
-		this.routes.forEach(route => {
+		this.routes().forEach(route => {
 			this.router[route.method](route.segment, ...route.middlewares)
 		})
 	}
