c466e85901c42e740352fe061c1eab6091adcf51
梁举恩 <3103421940@qq.com>
2023-12-17 22:24:44 +0800

    update ...

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,5 +1,7 @@
 import { AppModule as Module } from '@classes'
 import { IAppRoute } from '@interfaces'
+import { Guard } from './middlewares'
+
 import { UserRouter } from './modules/base/user/user.router'
 import { DictRouter } from './modules/dict/info/dict.router'
 import { DictTypeRouter } from './modules/dict/type/router'
@@ -8,22 +10,21 @@ import { LoginLogRouter } from './modules/loginLog/login-log.router'
 import { AuthRouter } from './modules/auth/auth.router'
 
 export class AppModule extends Module {
+	auth = Guard.checkJwt
 	routes(): IAppRoute[] {
 		return [
-			{ segment: '/v1', provider: AuthRouter, version: 'v1' },
-			{ segment: '/v1/users', auth: true, provider: UserRouter, version: 'v1' },
-			{ segment: '/v1/dicts', auth: true, provider: DictRouter, version: 'v1' },
+			{ segment: '/v1/auth', provider: AuthRouter },
+			{ segment: '/v1/users', auth: true, provider: UserRouter },
+			{ segment: '/v1/dicts', auth: true, provider: DictRouter },
 			{
 				segment: '/v1/dictTypes',
 				auth: true,
-				provider: DictTypeRouter,
-				version: 'v1'
+				provider: DictTypeRouter
 			},
 			{
 				segment: '/v1/loginLogs',
 				auth: true,
-				provider: LoginLogRouter,
-				version: 'v1'
+				provider: LoginLogRouter
 			}
 		]
 	}
