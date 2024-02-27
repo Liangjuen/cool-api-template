0cc55507c7233d91eba133c81d64dcce5491e84d
梁举恩 <3103421940@qq.com>
2024-02-27 21:33:03 +0800

    toSkipAndTake 函数新增返回值 'cPage'(当前页码)

--- src/api/shared/utils/query.ts
+++ src/api/shared/utils/query.ts
@@ -1,8 +1,10 @@
 export const toSkipAndTake = (page: string | number, size: string | number) => {
 	const take = parseInt(size as string, 10)
-	const skip = (parseInt(page as string, 10) - 1) * take
+	const cPage = parseInt(page as string, 10)
+	const skip = (cPage - 1) * take
 	return {
 		take,
-		skip
+		skip,
+		cPage
 	}
 }
