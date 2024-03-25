72bda608bca5aa245440686305b6cec80920bb43
梁举恩 <3103421940@qq.com>
2024-03-26 00:36:31 +0800

    🐞 fix: 完善用户修改密码逻辑

--- src/api/modules/base/user/person/person.service.ts
+++ src/api/modules/base/user/person/person.service.ts
@@ -1,26 +1,55 @@
-import { IRequest, IResponse } from '@interfaces'
+import { IResponse } from '@api/shared/interfaces'
 import { Resolve } from '@decorators'
+import { BadRequest } from '@exceptions'
 import { UserRepository } from '../user.repository'
+import { IUserRequest } from '../user.interface'
 
 export class PersonService {
 	@Resolve()
-	async get(req: IRequest, res: IResponse) {
+	async get(req: IUserRequest, res: IResponse) {
 		const repository = new UserRepository()
-		res.locals.data = await repository.one(parseInt(req.user.id, 10))
+		res.locals.data = await repository.one(req.user.id)
 	}
 
 	@Resolve()
-	async update(req: IRequest, res: IResponse) {
+	async update(req: IUserRequest, res: IResponse) {
 		const repository = new UserRepository()
-		const person = await repository.findOneBy({
-			id: parseInt(req.user.id, 10)
+		const { username, phone, email, nickName } = req.body
+		const { id } = req.user
+
+		await repository.checkIfFieldsExist({
+			username,
+			phone,
+			email,
+			nickName,
+			id
 		})
+
+		const person = await repository.findOneBy({
+			id: req.user.id
+		})
+
 		const newPerson = repository.merge(person, req.body)
 		res.locals.data = await repository.save(newPerson)
 	}
 
 	@Resolve()
-	async updatePassword(req: IRequest, res: IResponse) {
-		res.locals.data = {}
+	async updatePassword(req: IUserRequest, res: IResponse) {
+		const repository = new UserRepository()
+		const person = await repository
+			.createQueryBuilder('person')
+			.addSelect('person.password')
+			.where({ id: req.user.id })
+			.getOne()
+
+		const flog = await person.passwordMatches(req.body.oldPassword)
+
+		if (!flog) throw new BadRequest('旧密码错误')
+
+		const newPerson = repository.merge(person, { password: req.body.password })
+
+		await newPerson.hashPassword()
+
+		res.locals.data = await repository.save(newPerson)
 	}
 }
